8e4623e91e04f2b09186723fbfbab1d19d6fe021
Luis Antonio Momm Duarte <luis.aduarte@totvs.com.br>
2025-05-10 18:36:39 -0300

    Refactor main.c to streamline matrix operations and remove unused variables

--- main.c
+++ main.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include "matriz.h"
 #include <pthread.h>
-#include <assert.h>
 
 int main(int argc, char **argv)
 {
@@ -12,10 +11,7 @@ int main(int argc, char **argv)
     matriz_t *A = NULL;
     matriz_t *B = NULL;
     matriz_t *C = NULL;
-    matriz_t *C_sequencial = NULL;
     matriz_t *D = NULL;
-    matriz_t *B_transposta = NULL;
-    matriz_t *D_sequencial = NULL;
 
     if ((argc != 4)) {
         printf("Uso: %s <N> <NUM_THREADS> <PARALLEL_TYPE>\n", argv[0]);
@@ -28,33 +24,52 @@ int main(int argc, char **argv)
 
     A = matriz_criar(linhas, colunas); 
     B = matriz_criar(linhas, colunas); 
-    matriz_preencher_rand(B);
-    matriz_preencher_rand(A);
-    B_transposta = matriz_criar(colunas, linhas);
-    B_transposta = matriz_transpor(B);
+    D = matriz_criar(linhas, colunas);
 
+    matriz_preencher_rand(A);
+    matriz_preencher_rand(B);
+
+    //printf("Matriz A\n");
+    //matriz_imprimir(A);
+
+    //printf("Matriz B\n");
+    //matriz_imprimir(B);
+
+	// aqui criamos tudo
 	thread_params *parametros = NULL;
 	pthread_t *threads = NULL;
 	parametros = (thread_params*) malloc(sizeof(thread_params) * num_threads);
 	threads = (pthread_t*) malloc(sizeof(pthread_t) * num_threads);
 	
-    C_sequencial  = matriz_somar(A, B);
-    C = matriz_criar(linhas, colunas); 
-    D = matriz_criar(linhas, colunas);
-    D_sequencial = matriz_criar(linhas, colunas);
-    D_sequencial = matriz_multiplicar(A, B);
-    D = matriz_multiplicar_openmp(A, B);
+    	C = matriz_criar(linhas, colunas); 
+	for (int i = 0; i < num_threads; i++) {
+		parametros[i].tid = i;
+		parametros[i].A = A;
+		parametros[i].B = B;
+		parametros[i].C = C;
+        parametros[i].D = D;
+		parametros[i].num_threads = num_threads;
+		pthread_create(&threads[i], NULL, matriz_multiplicar_paralelo, &parametros[i]);
+	}
 
-    if (!matrizes_iguais(D, D_sequencial)) { fprintf(stderr, "Error: Matrices are not equal!\n");
-         exit(EXIT_FAILURE);
+    //C = matriz_somar(A, B);
+    for (int i = 0; i < num_threads; i++) {
+	    pthread_join(threads[i], NULL);
     }
 
+
+//    printf("Matriz C\n");
+//    matriz_imprimir(C);
+
+    D = matriz_multiplicar(A, B);
+
+/*    printf("Matriz D\n");
+    matriz_imprimir(D);
+*/
     matriz_destruir(A);
     matriz_destruir(B);
     matriz_destruir(C);
-    matriz_destruir(C_sequencial);
     matriz_destruir(D);
-    matriz_destruir(D_sequencial);
 
     return EXIT_SUCCESS;
 }

aa48fe0c6000dc684fc5eff06d98208f59708e7e
Luis Antonio Momm Duarte <luis.aduarte@totvs.com.br>
2025-06-10 21:32:53 -0300

    traduz variaveis

--- mp/mpreducer.cpp
+++ mp/mpreducer.cpp
@@ -8,11 +8,20 @@
 #include <algorithm>
 #include <queue>
 #include <filesystem>
+#include <optional>
 
+struct Task;
 const int COORDINATOR = 0;
 
 int getFileCount(int rank);
-std::pair<std::vector<Task>, std::vector<Task>> initializeTasks(int nMap, int nReduce);
+
+std::pair<std::vector<Task>, std::vector<Task> > initializeTasks(int nMap, int nReduce);
+
+Task receiveTask(int worker);
+
+std::optional<Task> selectTask(std::vector<Task> &tasks, int worker);
+
+void sendTaskToWorker(std::vector<Task> &tasks, int worker);
 
 struct Task {
     enum class Status {
@@ -21,6 +30,7 @@ struct Task {
         IN_PROGRESS,
         COMPLETED,
     };
+
     enum class Type {
         MAP,
         REDUCE
@@ -33,8 +43,25 @@ struct Task {
     int id;
 };
 
+std::ostream &operator<<(std::ostream &os, const Task::Status &status) {
+    switch (status) {
+        case Task::Status::NOT_ASSIGNED:
+            os << "NOT_ASSIGNED";
+            break;
+        case Task::Status::PENDING:
+            os << "PENDING";
+            break;
+        case Task::Status::IN_PROGRESS:
+            os << "IN_PROGRESS";
+            break;
+        case Task::Status::COMPLETED:
+            os << "COMPLETED";
+            break;
+    }
+    return os;
+}
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
     MPI_Init(&argc, &argv);
 
     int rank, size, nMap, nReduce;
@@ -48,28 +75,31 @@ int main(int argc, char** argv) {
 
     // todo mudar o numero de reducers e mappers para pegar do argumento
     nMap = getFileCount(rank);
-    nReduce = 2; 
+    nReduce = 2;
 
     auto [mapTasks, reduceTasks] = initializeTasks(nMap, nReduce);
-    
+
     if (rank == COORDINATOR) {
         while (nMap > 0) {
             int worker = idleWorkers.front();
             idleWorkers.pop();
-            Task task = selectTask(mapTasks, worker);
-            MPI_Send(&task, sizeof(Task), MPI_BYTE, worker, 0, MPI_COMM_WORLD);
+            if (idleWorkers.empty()) {
+                break;
+            }
+            sendTaskToWorker(mapTasks, worker);
             nMap--;
         }
-   
-        while (nReduce > 0) {
-            int worker = idleWorkers.front();
-            idleWorkers.pop();
-            Task task = selectTask(reduceTasks, worker);   
-            MPI_Send(&task, sizeof(Task), MPI_BYTE, worker, 0, MPI_COMM_WORLD);
-            nReduce--;
-        }   
-    }
-    else {
+
+        // while (nReduce > 0) {
+        //     int worker = idleWorkers.front();
+        //     idleWorkers.pop();
+        //     if (idleWorkers.empty()) {
+        //         break;
+        //     }
+        //     sendTaskToWorker(reduceTasks, worker);
+        //     nReduce--;
+        // }
+    } else {
         while (true) {
             Task task = receiveTask(rank);
             if (task.type == Task::Type::MAP) {
@@ -81,8 +111,8 @@ int main(int argc, char** argv) {
                     std::cout << line << std::endl;
                 }
                 file.close();
-            }
-            else {
+                idleWorkers.push(rank);
+            } else {
                 // reduce task 
             }
         }
@@ -90,40 +120,47 @@ int main(int argc, char** argv) {
 
 
     MPI_Finalize();
-    
 }
 
 int getFileCount(int rank) {
     int numFiles;
-    
+
     if (rank == COORDINATOR) {
         std::vector<std::string> files;
         std::string directory = "./files";
-        
+
         // List files in directory
-        for (const auto& entry : std::filesystem::directory_iterator(directory)) {
+        for (const auto &entry: std::filesystem::directory_iterator(directory)) {
             files.push_back(entry.path().string());
         }
-        
+
         numFiles = files.size();
         MPI_Bcast(&numFiles, 1, MPI_INT, COORDINATOR, MPI_COMM_WORLD);
     } else {
         MPI_Bcast(&numFiles, 1, MPI_INT, COORDINATOR, MPI_COMM_WORLD);
     }
-    
+
     return numFiles;
 }
 
-std::pair<std::vector<Task>, std::vector<Task>> initializeTasks(int nMap, int nReduce) {
+std::pair<std::vector<Task>, std::vector<Task> > initializeTasks(int nMap, int nReduce) {
     std::vector<Task> mapTasks(nMap);
     std::vector<Task> reduceTasks(nReduce);
+
+    std::vector<std::string> files;
+    std::string directory = "./files";
+
+    // List files in directory
+    for (const auto &entry: std::filesystem::directory_iterator(directory)) {
+        files.push_back(entry.path().filename().string());
+    }
 
     for (int i = 0; i < nMap; i++) {
         mapTasks[i] = Task{
             .status = Task::Status::NOT_ASSIGNED,
             .type = Task::Type::MAP,
             .index = i,
-            .file = "",
+            .file = files[i % files.size()],
             .id = i
         };
     }
@@ -141,48 +178,68 @@ std::pair<std::vector<Task>, std::vector<Task>> initializeTasks(int nMap, int nR
     return {mapTasks, reduceTasks};
 }
 
-Task selectTask(std::vector<Task>& tasks, int worker) {
-    for (auto& task : tasks) {
+std::optional<Task> selectTask(std::vector<Task> &tasks, int worker) {
+    for (auto &task: tasks) {
         if (task.status == Task::Status::NOT_ASSIGNED) {
             task.status = Task::Status::PENDING;
             task.id = worker;
-            return task;
+            return std::make_optional(task);
         }
     }
-    throw std::runtime_error("No task found");
+    return std::nullopt;
 }
 
-void sendTask(Task& task, int worker) {
-    int status = task.status;
-    MPI_Send(&status, sizeof(int), MPI_INT, worker, 0, MPI_COMM_WORLD);
-    //index 
-    MPI_Send(&task.index, sizeof(int), MPI_INT, worker, 1, MPI_COMM_WORLD);
-    //file
-    MPI_Send(&task.file, sizeof(std::string), MPI_CHAR, worker, 2, MPI_COMM_WORLD);
-    //id
-    MPI_Send(&task.id, sizeof(int), MPI_INT, worker, 3, MPI_COMM_WORLD);
+void sendTaskToWorker(std::vector<Task> &tasks, int worker) {
+    std::cout << "Sending map task to worker " << worker << std::endl;
+    auto task = selectTask(tasks, worker);
+    if (!task.has_value()) {
+        return;
+    }
 
-    // send file location 
-    std::string fileLocation = "./files/" + task.file;
-    MPI_Send(&fileLocation, sizeof(std::string), MPI_CHAR, worker, 4, MPI_COMM_WORLD);
+    std::cout << "sent Status: " << task.value().status << std::endl;
+    MPI_Send(&task.value().status, sizeof(int), MPI_INT, worker, 0, MPI_COMM_WORLD);
+    std::cout << "Index: " << task.value().index << std::endl;
+    MPI_Send(&task.value().index, sizeof(int), MPI_INT, worker, 1, MPI_COMM_WORLD);
+    std::cout << "File: " << task.value().file << std::endl;
+    const char *fileCharArray = task.value().file.c_str();
+    MPI_Send(fileCharArray, task.value().file.size() + 1, MPI_CHAR, worker, 2, MPI_COMM_WORLD);
+    std::cout << "Id: " << task.value().id << std::endl;
+    MPI_Send(&task.value().id, sizeof(int), MPI_INT, worker, 3, MPI_COMM_WORLD);
+
+    std::string fileLocation = "./files/" + task.value().file;
+    std::cout << "File location: " << fileLocation << std::endl;
+    const char *fileLocationCharArray = fileLocation.c_str();
+    MPI_Send(fileLocationCharArray, fileLocation.size() + 1, MPI_CHAR, worker, 4, MPI_COMM_WORLD);
+    std::cout << "File location sent" << std::endl;
 }
 
 Task receiveTask(int worker) {
+    std::cout << "Receiving task from coordinator" << std::endl;
     int status;
-    MPI_Recv(&status, sizeof(int), MPI_INT, worker, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-    
-    MPI_Recv(index, sizeof(int), MPI_INT, worker, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-    MPI_Recv(file, sizeof(std::string), MPI_CHAR, worker, 2, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-    MPI_Recv(id, sizeof(int), MPI_INT, worker, 3, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    MPI_Recv(&status, sizeof(int), MPI_INT, COORDINATOR, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    std::cout << "received Status: " << status << std::endl;
 
-    std::string fileLocation;
-    MPI_Recv(&fileLocation, sizeof(std::string), MPI_CHAR, worker, 4, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-    file = fileLocation.substr(fileLocation.find_last_of('/') + 1);
+    int index;
+    std::string file;
+    int id;
+    MPI_Recv(&index, sizeof(int), MPI_INT, COORDINATOR, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    std::cout << "received Index: " << index << std::endl;
+    
+    char fileCharArray[256];
+    MPI_Recv(fileCharArray, 256, MPI_CHAR, COORDINATOR, 2, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    std::cout << "received File: " << fileCharArray << std::endl;
+    file = std::string(fileCharArray);
+    
+    MPI_Recv(&id, sizeof(int), MPI_INT, COORDINATOR, 3, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+    char fileLocationCharArray[256];
+    MPI_Recv(fileLocationCharArray, 256, MPI_CHAR, COORDINATOR, 4, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    std::cout << "received File location: " << fileLocationCharArray << std::endl;
 
     return Task{
         .status = static_cast<Task::Status>(status),
+        .type = Task::Type::MAP,
         .index = index,
-
         .file = file,
         .id = id
     };
